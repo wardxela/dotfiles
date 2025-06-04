1a9f345fdc9b9ea8aaf9bbf62176cde331769dae
wardxela <wardxela@gmail.com>
2025-06-04 04:00:50 +0300

    i am not a designer

--- modules/shared/programs.nix
+++ modules/shared/programs.nix
@@ -23,27 +23,6 @@
       in
       {
         enable = true;
-        colorScheme = "custom";
-        customColorScheme = {
-          text = "F0F6FC";
-          subtext = "F0F6FC";
-          main = "0D1117";
-          main-elevated = "0D1117";
-          highlight = "1F232A";
-          highlight-elevated = "15191F";
-          sidebar = "010409";
-          player = "010409";
-          card = "010409"; # What this color does?
-          shadow = "0D1117";
-          selected-row = "F0F6FC";
-          button = "4493F8";
-          button-active = "4493F8";
-          button-disabled = "212830";
-          tab-active = "010409";
-          notification = "4493F8";
-          notification-error = "F85149";
-          misc = "010409"; # What this color does?
-        };
         enabledExtensions = with spicePkgs.extensions; [
           adblockify
           shuffle
