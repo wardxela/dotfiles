37cdf027644f04871a417dfecc68d301c1cf1fe0
wardxela <wardxela@gmail.com>
2025-07-07 21:09:23 +0300

    docs: todo

--- modules/linux/packages.nix
+++ modules/linux/packages.nix
@@ -29,8 +29,8 @@
     gcr
 
     # TODO: Should be in the /shared/packages
-    anytype # can't compile
-    ghostty # marked as broken
+    anytype # https://github.com/NixOS/nixpkgs/pull/411491
+    ghostty # https://github.com/NixOS/nixpkgs/issues/404535#issuecomment-2853021600
     jan # lack of support
     exodus # lack of support
   ];
