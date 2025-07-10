fb35037950b31dc377e253e2fda7304ef41b34c1
wardxela <wardxela@gmail.com>
2025-07-11 01:06:40 +0300

    refactor: programs

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -3,6 +3,8 @@
 {
   environment.systemPackages = with pkgs; [
     # GUI
+    # TODO: https://github.com/NixOS/nixpkgs/issues/401074
+    # amnezia-vpn
     monitorcontrol
     iina
     keka
--- modules/linux/programs.nix
+++ modules/linux/programs.nix
@@ -1,15 +1,17 @@
 { pkgs, ... }:
 
 {
-  programs.amnezia-vpn.enable = true;
-  programs.nix-ld.enable = true;
-  programs.gnupg = {
-    agent = {
-      enable = true;
-      pinentryPackage = pkgs.pinentry-tty;
+  programs = {
+    amnezia-vpn.enable = true;
+    nix-ld.enable = true;
+    gnupg = {
+      agent = {
+        enable = true;
+        pinentryPackage = pkgs.pinentry-tty;
+      };
     };
+    steam.enable = true;
+    # TODO: Conflicts with gnome-keyring
+    # ssh.startAgent = true;
   };
-  programs.steam.enable = true;
-  # TODO: Conflicts with gnome-keyring
-  # programs.ssh.startAgent = true;
 }
