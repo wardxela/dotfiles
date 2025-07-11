3437a7f079576243bf9f3ca87a6e6a3311dbf74d
wardxela <wardxela@gmail.com>
2025-07-11 13:05:27 +0300

    fix: remove exodus since it is unusable at this point

--- modules/linux/packages.nix
+++ modules/linux/packages.nix
@@ -25,6 +25,6 @@
     # TODO: Should be in the /shared/packages
     anytype # https://github.com/NixOS/nixpkgs/pull/411491
     jan # lack of support
-    exodus # lack of support
+    # exodus # lack of support + outdated
   ];
 }
