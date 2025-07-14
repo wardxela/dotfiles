7c9e9dff465672c9b137acec7d4ee1a31178aa7e
wardxela <wardxela@gmail.com>
2025-07-14 14:43:33 +0300

    feat: launch termusic during tmux session instantiation

--- scripts/tmx.sh
+++ scripts/tmx.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-if tmux has-session -t work 2>/dev/null; then
-  tmux attach-session -t work
+SESSION_NAME="tmux"
+
+if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+  tmux attach-session -t"$SESSION_NAME"
 else
-  tmux new-session -d -s work
-  tmux rename-window -t 1 app
-  tmux new-window -d -n sh
-  tmux attach-session -d -t work
+  tmux new-session -d -s "$SESSION_NAME"
+  tmux rename-window -t 1 main
+  tmux new-window -d -n shell -t 2
+  tmux new-window -d -n shell -t 3
+  tmux new-window -d -n shell -t 4
+  tmux new-window -d -n music -t 5
+  tmux send-keys -t $SESSION_NAME:5 'termusic' C-m
+  tmux attach-session -d -t "$SESSION_NAME"
 fi
