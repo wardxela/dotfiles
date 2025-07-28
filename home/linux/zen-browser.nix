119294f06be7b414c1b38d2055bb4499d9a3619b
wardxela <wardxela@gmail.com>
2025-07-28 21:51:42 +0300

    feat(zen-browser): apply stylix and tweak some policies

--- home/linux/zen-browser.nix
+++ home/linux/zen-browser.nix
@@ -8,13 +8,23 @@
   programs.zen-browser = {
     enable = true;
     policies = {
+      AutofillAddressEnabled = false;
       AutofillCreditCardEnabled = false;
       DisableAppUpdate = true;
       DisableFeedbackCommands = true;
       DisableFirefoxStudies = true;
+      DisableFormHistory = true;
       DisablePocket = true;
       DisableTelemetry = true;
       OfferToSaveLogins = false;
+      ExtensionSettings = {
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
     };
+    profiles.wardxela = { };
   };
+  stylix.targets.zen-browser.profileNames = [ "wardxela" ];
 }
