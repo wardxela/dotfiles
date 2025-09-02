eeb0d63c27ec6fc9f5640f8330b184829d8856e0
wardxela <wardxela@gmail.com>
2025-09-03 00:44:57 +0300

    fix(gtk): remove useless settings

--- home/linux/gtk.nix
+++ home/linux/gtk.nix
@@ -1,27 +1,11 @@
 { pkgs, ... }:
 
 {
-  home = {
-    # gtk applications should use xdg specified settings
-    sessionVariables = {
-      GTK_USE_PORTAL = "1";
-    };
-  };
   gtk = {
     enable = true;
     iconTheme = {
       name = "Papirus";
       package = pkgs.papirus-icon-theme;
-    };
-    gtk3 = {
-      extraConfig = {
-        gtk-decoration-layout = "appmenu:none";
-      };
-    };
-    gtk4 = {
-      extraConfig = {
-        gtk-decoration-layout = "appmenu:none";
-      };
     };
   };
 }
