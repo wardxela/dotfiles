c2a0214fcacfddc085f2db73d4dcf46467c389af
wardxela <wardxela@gmail.com>
2025-09-08 16:41:36 +0300

    fix(linux): xdg issues

--- modules/linux/default.nix
+++ modules/linux/default.nix
@@ -35,10 +35,16 @@
 
   xdg.portal = {
     enable = true;
-    configPackages = with pkgs; [
+    config.sway = {
+      default = [ "gtk" ];
+      "org.freedesktop.impl.portal.Screenshot" = "wlr";
+      "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+    };
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
       xdg-desktop-portal-wlr
-      xdg-desktop-portal-shana
     ];
+    xdgOpenUsePortal = true;
   };
 
   security.polkit.enable = true;
